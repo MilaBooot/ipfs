bd94903be0007144976b0f3985fbfea4260d6c9b
menilik eshetu <spyxmeni@gmail.com>
2024-10-16 11:54:38 +0300

    upload and download ipfs file structre with pinata

---
+++ Readme.md
@@ -0,0 +1,8 @@
+First activate virtual environment
+Create a pinata free account with Developers mode
+Generate API key from Pinata and store the key in environmental varaibles
+Second install the requirements including
+
+pip install requests
+pip install python-dotenv
+pip install pprintpp
---
+++ ipfs_test_download.py
@@ -0,0 +1,21 @@
+import requests
+import os
+from dotenv import load_dotenv
+import pprint
+
+# Load environment variables
+load_dotenv()
+
+
+def get_from_pinata(jwt_token):
+    url = "https://api.pinata.cloud/data/pinList"
+    headers = {'Authorization': f'Bearer {jwt_token}'}
+
+    response = requests.get(url, headers=headers)
+
+    return response.json()
+    
+
+PINATA_JWT_TOKEN = os.getenv('PINATA_JWT_TOKEN')
+
+pprint.pprint(get_from_pinata(PINATA_JWT_TOKEN))
